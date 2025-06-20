95edbd7344cb01331c25e440cc13e59a8b22c56b
shiron <shiron4710.dev@gmail.com>
2025-06-20 15:01:56 +0900

    refactor: clean up variables.tf by removing deprecated variable definitions

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,21 +1,5 @@
-# Variables for GCP Pub/Sub CI/CD infrastructure
-# Note: project_id, region, and zone are now defined in locals block in main.tf
-
-variable "environment" {
-  description = "Environment name (e.g., dev, staging, prod)"
-  type        = string
-  default     = "prod"
-}
-
 variable "github_token" {
   description = "GitHub Personal Access Token for kizuna-org organization with read:packages scope for GHCR pull access"
   type        = string
   sensitive   = true
 }
-
-# Note: The following variables have been moved to locals in main.tf:
-# - build_topic_name
-# - app_topic_name
-# - build_subscription_name
-# - app_subscription_name
-# - service_account_name
