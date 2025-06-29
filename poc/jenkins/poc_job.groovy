f7660d9ffeb77210de24dad281883b957e5c1c68
shiron <shiron4710.dev@gmail.com>
2025-06-29 21:21:04 +0900

    fix: complete Job DSL syntax correction for poc_job.groovy
    - Fix gitea() method error by using git() instead
    - Correct branchSource structure with proper nesting
    - Use gitBranchDiscovery() instead of branchDiscoveryTrait()
    - Fix orphanedItemStrategy syntax to use discardOldItems
    - Add comprehensive documentation and implementation details
    - Successfully create my-poc-repo multibranch pipeline job

--- poc/jenkins/poc_job.groovy
+++ poc/jenkins/poc_job.groovy
@@ -2,12 +2,22 @@ multibranchPipelineJob('my-poc-repo') {
     description('Multibranch pipeline for POC repository')
 
     branchSources {
-        git {
-            id('ea86f402-26ae-44a2-a857-c0ccc5c30a4d')
-            remote('http://gitea:3000/poc_user/my-poc-repo.git')
-            credentialsId('gitea-credentials')
-            traits {
-                branchDiscoveryTrait()
+        branchSource {
+            source {
+                git {
+                    id('ea86f402-26ae-44a2-a857-c0ccc5c30a4d')
+                    remote('http://gitea:3000/poc_user/my-poc-repo.git')
+                    credentialsId('gitea-credentials')
+                    traits {
+                        gitBranchDiscovery()
+                    }
+                }
+            }
+            strategy {
+                defaultBranchPropertyStrategy {
+                    props {
+                    }
+                }
             }
         }
     }
