6cbf6cda5b47945b4a575198ec351eae942fcc81
shiron <shiron4710.dev@gmail.com>
2025-06-29 18:40:49 +0900

    chore: update Jenkins configuration and remove obsolete files
    - Added `casc.yaml` to `compose.yaml` for improved Jenkins configuration management.
    - Deleted `extract_jenkins_config.sh` and `job_dsl_examples.groovy` as they are no longer needed.
    - Removed `asr_poc_job_dsl.groovy` and `temp_gitea_work` to clean up unused resources.
    - Updated `plugins.txt` to include `xml-job-to-job-dsl` plugin for enhanced Job DSL capabilities.

---
+++ poc/jenkins/poc-job.groovy
@@ -0,0 +1,26 @@
+multibranchPipelineJob('my-poc-repo') {
+    description('')
+
+    branchSources {
+        git {
+            id('ea86f402-26ae-44a2-a857-c0ccc5c30a4d')
+            remote('http://gitea:3000/poc_user/my-poc-repo.git')
+            credentialsId('gitea-credentials')
+            traits {
+                branchDiscovery()
+            }
+        }
+    }
+
+    orphanedItemStrategy {
+        pruneDeadBranches(true)
+        daysToKeep(-1)
+        numToKeep(-1)
+    }
+
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
