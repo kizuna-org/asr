51475a8d95d58a26f2f0d229be4b19b6baab7a12
shiron <shiron4710.dev@gmail.com>
2025-07-04 14:13:09 +0900

    feat: enhance run script to manage Docker containers
    - Added functionality to stop and remove existing Docker containers running the ljspeech-tensorflow image before starting a new training session.
    - Improved the automation of the deployment process by ensuring no conflicting containers are left running, enhancing resource management during training.

--- run.sh
+++ run.sh
@@ -3,6 +3,12 @@
 scp ./Dockerfile edu-gpu:~/temp/
 rsync -avz --exclude 'outputs' --exclude 'datasets' --exclude '__pycache__' ./scripts/ edu-gpu:~/temp/
 
+DOCKER_PS=$(ssh edu-gpu "sudo docker ps -q --filter ancestor=ljspeech-tensorflow")
+for CONTAINER_ID in $DOCKER_PS; do
+    ssh edu-gpu "sudo docker stop $CONTAINER_ID"
+    ssh edu-gpu "sudo docker rm $CONTAINER_ID"
+done
+
 ssh edu-gpu "cd temp/ && ./build.sh"
 DOCKER_ID=$(ssh edu-gpu "cd temp/ && sudo docker run -d --gpus all -v \$(pwd)/outputs:/opt/outputs ljspeech-tensorflow")
 ssh edu-gpu "sudo docker logs -f $DOCKER_ID"
