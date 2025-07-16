962a5441c75663a554237e9e50e62f8b8ac199fe
shiron <shiron4710.dev@gmail.com>
2025-07-16 14:07:50 +0900

    wip

--- infra/files/docker_pre_build.sh
+++ infra/files/docker_pre_build.sh
@@ -2,12 +2,20 @@
 
 set -euo pipefail
 
+trap 'echo "[kizuna-org/asr]Finished building docker images"' EXIT
+
+pids=()
 for dir in */ ; do
   if [ -f "${dir}Dockerfile" ]; then
     tag=$(basename "$dir")
     (
       cd "$dir"
-      sudo docker build -t "$tag" .
-    )
+      sudo docker build --network host -t "$tag" .
+    ) &
+    pids+=($!)
   fi
+done
+
+for pid in "${pids[@]}"; do
+  wait "$pid"
 done
